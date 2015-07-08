d70fb96167d005de5e6452c3f5030404e870bde1
Bruno Santos <btsantos@gmail.com>
2015-07-08 16:20:57 +0100

    Create slot.R

---
+++ slot.R
@@ -0,0 +1,79 @@
+
+get_symbols <- function() {
+  wheel <- c("DD", "7", "BBB", "BB", "B", "C", "0")
+  sample(wheel, size = 3, replace = TRUE,
+         prob = c(0.03, 0.03, 0.06, 0.1, 0.25, 0.01, 0.52))
+}
+
+score_no_wdd <- function (symbols) {
+  # identify case
+  same <- symbols[1] == symbols[2] && symbols[2] == symbols[3]
+  bars <- symbols %in% c("B", "BB", "BBB")
+  # get prize
+  if (same) {
+    payouts <- c("DD" = 100, "7" = 80, "BBB" = 40, "BB" = 25, "B" = 10, "C" = 10, "0" = 0)
+    prize <- unname(payouts[symbols[1]])
+  } else if (all(bars)) {
+    prize <- 5
+  } else {
+    cherries <- sum(symbols == "C")
+    prize <- c(0, 2, 5)[cherries + 1]
+  }
+  # adjust for diamonds
+  diamonds <- sum(symbols == "DD")
+  prize * 2 ^ diamonds
+}
+
+score <- function(symbols) {
+  diamonds <- sum(symbols == "DD")
+  cherries <- sum(symbols == "C")
+  # identify case
+  # since diamonds are wild, only nondiamonds
+  # matter for three of a kind and all bars
+  slots <- symbols[symbols != "DD"]
+  same <- length(unique(slots)) == 1
+  bars <- slots %in% c("B", "BB", "BBB")
+  # assign prize
+  if (diamonds == 3) {
+    prize <- 100
+  } else if (same) {
+    payouts <- c("7" = 80, "BBB" = 40, "BB" = 25,
+                 "B" = 10, "C" = 10, "0" = 0)
+    prize <- unname(payouts[slots[1]])
+  } else if (all(bars)) {
+    prize <- 5
+  } else if (cherries > 0) {
+    # diamonds count as cherries
+    # so long as there is one real cherry
+    prize <- c(0, 2, 5)[cherries + diamonds + 1]
+  } else {
+    prize <- 0
+  }
+  # double for each diamond
+  prize * 2^diamonds
+}
+
+play <- function() {
+  symbols <- get_symbols()
+  ## print(symbols)
+  #prize <- score(symbols)
+  #attr(prize, "symbols") <- symbols
+  #prize
+  structure(score(symbols), symbols = symbols, class = "slots")
+}
+
+slot_display <- function(prize){
+  # extract symbols
+  symbols <- attr(prize, "symbols")  ## "B" "0" "B"
+  # collapse symbols into single string
+  symbols <- paste(symbols, collapse = " ") ## "B 0 B"
+  # combine symbol with prize as a regular expression
+  # \ n is regular expression for new line (i.e. return or enter)
+  string <- paste(symbols, prize, sep = "\n$") "B 0 B\n$0"
+  # like print display regular expression in console without quotes and replaces every \n with a new line or line break.
+  cat(string)
+}
+
+print.slots <- function(x, ...) {
+  slot_display(x)
+}
